465f4c6518897cbc6b16654137764c9a8181f2a7
git-uday-kiran <udaykiran0486@gmail.com>
2024-08-02 16:58:58 +0530

    created project in intellij

---
+++ src/main/java/links/backend/LinksBackendApplication.java
@@ -0,0 +1,13 @@
+package links.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinksBackendApplication.class, args);
+    }
+
+}
