6eca79fb2bcc68d59cd82ed527da668a6cb57780
git-uday-kiran <udaykiran0486@gmail.com>
2024-08-06 17:18:27 +0530

    removed bean post processor

--- src/main/java/links/backend/LinksBackendApplication.java
+++ src/main/java/links/backend/LinksBackendApplication.java
@@ -1,9 +1,5 @@
 package links.backend;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,19 +12,6 @@ public class LinksBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinksBackendApplication.class, args);
-	}
-
-
-//	@Bean
-	BeanPostProcessor beanPostProcessor() {
-		return new BeanPostProcessor() {
-			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-				if (bean instanceof ObjectMapper mapper) {
-					mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-				}
-				return bean;
-			}
-		};
 	}
 
 	@Bean
